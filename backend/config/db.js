2c63b3dc0c5662b3ef6a28835f50eb7944ee2435
Björn Tenje Persson <bjorn@tenje.se>
2024-12-29 22:21:57 +0000

    Made .env file more useful

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('systembolaget_priser', 'user', 'password', {
-  host: 'mysql_container',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   pool: {
     max: 10,
