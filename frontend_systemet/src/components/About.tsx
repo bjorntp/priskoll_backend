8510dec01f547224c8efce64e2d23d71a7da1b27
Björn Tenje Persson <bjorn@tenje.se>
2025-01-08 09:52:15 +0100

    Added filtering based on order-assortment

--- frontend_systemet/src/components/About.tsx
+++ frontend_systemet/src/components/About.tsx
@@ -5,21 +5,22 @@ const AboutComponent = () => {
         <div className="py-9 px-9">
           <h1 className="text-2xl pb-2">Hejsan!</h1>
           <p className="pb-2">
-            Välkommen hit, detta är en förhållandevis enkel hemsida skapad framförallt för att jag själv vill kunna fynda lite bätre viner när dess priser. </p>
-          <p className="pb-2">
-            Systembolaget presenterar inte själva när de sänker sina priser, rimligt eftersom det antagligen hade stridit mot deras <a className="underline underline-offset-2" href="https://www.omsystembolaget.se/vart-uppdrag/">uppdrag.</a>
+            Välkommen hit! Detta är en relativt enkel hemsida som jag har skapat främst för att själv kunna hitta bättre viner när deras priser sänks.
           </p>
           <p className="pb-2">
-            Sidan fungerar genom att med jämna mellanrum hämta all data från Systembolagets API och jämföra den med datan som redan finns i databasen. På sidorna visas endast de produkter vars pris har förändrats med minst 10 %.
+            Systembolaget informerar inte om när de sänker sina priser, vilket är förståeligt eftersom det skulle kunna strida mot deras <a className="underline underline-offset-2" href="https://www.omsystembolaget.se/vart-uppdrag/">uppdrag</a>.
           </p>
           <p className="pb-2">
-            Då Systembolaget bara tillåter leverantörer att ändra sina priser fyra gånger om året så finns där inte anledning till att uppdatera datan speciellt ofta, här kör jag en gång i veckan.
+            Sidan fungerar genom att regelbundet hämta all data från Systembolagets API och jämföra den med befintlig data i databasen. På sidan visas endast de produkter vars pris har förändrats med minst 10 %.
           </p>
           <p className="pb-2">
-            För att hämta datan så använder jag mig av ett API som går att hitta <a className="underline underline-offset-2" href="https://github.com/AlexGustafsson/systembolaget-api">här</a>. Sidans källkod är helt öppen och går att hitta på min <a className="underline underline-offset-2" href="https://github.com/bjorntp/priskoll_systemet">GitHub</a>. Där finns mycket som hade kunnat göras mer till sidan, till viss del vill jag hålla den så enkel som möjligt samtidigt som jag inte riktigt har tid just nu.
+            Eftersom Systembolaget endast tillåter leverantörer att ändra sina priser fyra gånger per år, finns det ingen anledning att uppdatera datan särskilt ofta. Därför kör jag uppdateringen en gång i veckan.
+          </p>
+          <p className="pb-2">
+            För att hämta datan använder jag ett API som du kan hitta <a className="underline underline-offset-2" href="https://github.com/AlexGustafsson/systembolaget-api">här</a>. Sidans källkod är helt öppen och finns tillgänglig på min <a className="underline underline-offset-2" href="https://github.com/bjorntp/priskoll_systemet">GitHub</a>. Det finns mycket som skulle kunna förbättras med sidan. Jag vill dock hålla den enkel och har inte riktigt tid att göra allt just nu.
           </p>
           <p>
-            Om du vill komma i kontakt med mig så går det hitta min kontaktinformation  <a className="underline underline-offset-2" href="https://tenje.se">här</a>.
+            Om du vill komma i kontakt med mig hittar du min kontaktinformation <a className="underline underline-offset-2" href="https://tenje.se">här</a>.
           </p>
         </div>
       </div>
--- frontend_systemet/src/components/LoweredComponent.tsx
+++ frontend_systemet/src/components/LoweredComponent.tsx
@@ -8,6 +8,7 @@ const LoweredComponent = () => {
   const location = useLocation();
   const [sort, setSort] = useState("alphabetical");
   const [expandIndex, setExpandIndex] = useState<number | null>(null);
+  const [orderFilter, setOrderFilter] = useState(false);
 
   const getData = async () => {
     try {
@@ -42,73 +43,90 @@ const LoweredComponent = () => {
     }
   }, [sort]);
 
+  const handleCheckBoxChange = () => {
+    setOrderFilter(!orderFilter);
+  }
+
   return (
-    <div className="bg-gradient-to-br from-baby via-sky-300 to-grotto h-screen w-screen sm:flex sm:flex-col">
-      <div className={`sm:rounded-xl bg-grotto shadow-lg transform transition-all duration-1000 h-full w-full sm:h-5/6 sm:w-2/3 m-auto flex flex-col justify-around items-center`} >
-        <div className="sticky top-0 z-10 w-full flex flex-col sm:flex-row justify-between items-center px-6 py-2 bg-navy text-baby sm:rounded-xl">
-          <h2 className="text-xl font-bold">Prissänkningar</h2>
-          <div className="flex flex-col sm:flex-row items-center pb-3 sm:pb-0">
-            <label htmlFor="sort" className="invisible sm:visible mr-2">
-              Sortering
-            </label>
-            <select
-              id="sort"
-              className="px-4 py-2 border rounded-md bg-white text-grotto"
-              value={sort}
-              onChange={(e) => handleSortChange(e.target.value)}
-            >
-              {sortingOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
+    <div className="bg-gradient-to-br from-baby via-sky-300 to-grotto h-screen w-screen md:flex md:flex-col">
+      <div className={`md:rounded-xl bg-grotto shadow-lg transform transition-all duration-1000 h-full w-full md:h-5/6 md:w-2/3 m-auto flex flex-col justify-around items-center`} >
+        <div className="sticky top-0 z-10 w-full items-center px-6 py-2 bg-navy text-baby md:rounded-xl">
+          <div className="grid grid-cols-6 grid-rows-3 md:grid-rows-2 items-center">
+            <h2 className="text-xl text-center font-bold row-start-1 col-start-1 col-span-6">Prissänkningar</h2>
+            <div className="py-1 row-start-2 col-start-1 col-span-6 md:col-span-3 flex flex-col md:flex-row items-center justify-center">
+              <select
+                id="sort"
+                className="px-4 py-2 border rounded-md bg-white text-grotto"
+                value={sort}
+                onChange={(e) => handleSortChange(e.target.value)}
+              >
+                {sortingOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="row-start-3 md:row-start-2 col-start-1 md:col-start-1 text-center md:col-span-3 col-span-6 justify-center items-center">
+              <label htmlFor="order" className="px-2">
+                Inkludera beställningsvaror (varierande leveranstid)
+              </label>
+              <input
+                id="order"
+                type="checkbox"
+                checked={orderFilter}
+                onChange={handleCheckBoxChange}
+                className="pl-2"
+              />
+            </div>
           </div>
         </div>
         <ul className="overflow-auto w-full py-2 divide-y divide-navy">
           {data.length > 0 ? (
             data.map((object, index) => (
-              <li key={index} className="">
-                <div className="p-2 px-6 sm:grid sm:grid-cols-6 text-baby" onClick={() => {
-                  if (expandIndex === index) {
-                    setExpandIndex(null);
-                  } else {
-                    setExpandIndex(index);
-                  }
-                }}>
-                  <p>{object.Product.productNameBold}</p>
-                  <p className="sm:col-span-2">{object.Product.productNameThin}</p>
-                  <p className="">{object.Product.categoryLevel2}</p>
-                  <p className="sm:text-end line-through text-red">{object.OldPrices[0].oldPrice}</p>
-                  <p className="sm:text-end sm:col-start-6">{object.Product.price}</p>
-                </div>
-                {expandIndex === index &&
-                  <div className="bg-baby text-grotto p-4">
-                    <p><strong>Typ:</strong> {object.Product.categoryLevel2}</p>
-                    <p><strong>Styrka:</strong> {object.Product.alcoholPercentage}</p>
-                    <p><strong>Volym:</strong> {object.Product.volume} ml</p>
-                    <p>
-                      <strong>Ursprung: </strong>
-                      {object.Product.country}
-                      {object.Product.originLevel1 ? `, ${object.Product.originLevel1}` : ""}
-                      {object.Product.originLevel2 ? `, ${object.Product.originLevel2}` : ""}
-                    </p>
-                    <p><strong>Producent: </strong>{object.Product.producerName}</p>
-                    {object.Product.grapes?.length > 0 && (
-                      <p>
-                        <strong>Druvor:</strong> {object.Product.grapes.join(", ")}
-                      </p>
-                    )}
-                    <p><a target="_blank" href={`https://www.systembolaget.se/produkt/${object.Product.categoryLevel1}/${object.Product.productNumber}`}>Sida hos Systembolaget</a></p>
+              ((object.Product.isBsAssortment === false || orderFilter) ? (
+                <li key={index} className="">
+                  <div className="p-2 px-6 md:grid md:grid-cols-6 text-baby" onClick={() => {
+                    if (expandIndex === index) {
+                      setExpandIndex(null);
+                    } else {
+                      setExpandIndex(index);
+                    }
+                  }}>
+                    <p>{object.Product.productNameBold}</p>
+                    <p className="md:col-span-2">{object.Product.productNameThin}</p>
+                    <p className="">{object.Product.categoryLevel2}</p>
+                    <p className="md:text-end line-through text-red">{object.OldPrices[0].oldPrice}</p>
+                    <p className="md:text-end md:col-start-6">{object.Product.price}</p>
                   </div>
-                }
-              </li>
+                  {expandIndex === index &&
+                    <div className="bg-baby text-grotto p-4">
+                      <p><strong>Typ:</strong> {object.Product.categoryLevel2}</p>
+                      <p><strong>Styrka:</strong> {object.Product.alcoholPercentage}</p>
+                      <p><strong>Volym:</strong> {object.Product.volume} ml</p>
+                      <p>
+                        <strong>Ursprung: </strong>
+                        {object.Product.country}
+                        {object.Product.originLevel1 ? `, ${object.Product.originLevel1}` : ""}
+                        {object.Product.originLevel2 ? `, ${object.Product.originLevel2}` : ""}
+                      </p>
+                      <p><strong>Producent: </strong>{object.Product.producerName}</p>
+                      {object.Product.grapes?.length > 0 && (
+                        <p>
+                          <strong>Druvor:</strong> {object.Product.grapes.join(", ")}
+                        </p>
+                      )}
+                      <p><a target="_blank" href={`https://www.systembolaget.se/produkt/${object.Product.categoryLevel1}/${object.Product.productNumber}`}>Sida hos Systembolaget</a></p>
+                    </div>
+                  }
+                </li>
+              ) : (<></>)))
             ))
-          ) : (
-            <div className="h-full w-full m-auto">
-              <img className='text-navy w-32 h-32 m-auto animate-spin' src={reload} alt="Loading" />
-            </div>
-          )}
+            : (
+              <div className="h-full w-full m-auto">
+                <img className='text-navy w-32 h-32 m-auto animate-spin' src={reload} alt="Loading" />
+              </div>
+            )}
         </ul>
       </div>
     </div >
