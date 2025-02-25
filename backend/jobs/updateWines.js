9621ee39f3b7c1cc602c7bcc6e5b9fffd0593346
Björn Tenje Persson <bjorn@tenje.se>
2025-02-25 15:57:08 +0100

    Split routes
    Still tempory but split red, white and the rest of them since they are
    too big

--- backend/jobs/updateWines.js
+++ backend/jobs/updateWines.js
@@ -1,14 +1,10 @@
 const updateData = require('../controllers/updateData')
 
-const getWines = async (req, res) => {
+const getRestWine = async (req, res) => {
   try {
-    console.log("Updating wine data")
+    console.log("Updating non red and white wine data")
     let argsMousserande = ['assortment', '--category', "Vin", '--subcategory', 'Mousserande vin'];
     await updateData(argsMousserande);
-    let argsVitt = ['assortment', '--category', "Vin", '--subcategory', 'Vitt vin'];
-    await updateData(argsVitt);
-    let argsRott = ['assortment', '--category', "Vin", '--subcategory', 'Rött vin'];
-    await updateData(argsRott);
     let argsRose = ['assortment', '--category', "Vin", '--subcategory', 'Rosevin'];
     await updateData(argsRose);
     let argsLada = ['assortment', '--category', "Vin", '--subcategory', 'Vinlada'];
@@ -31,4 +27,29 @@ const getWines = async (req, res) => {
   }
 }
 
-module.exports = getWines;
+const getWhite = async (req, res) => {
+  try {
+    console.log("Updating white wine data")
+    let argsVitt = ['assortment', '--category', "Vin", '--subcategory', 'Vitt vin'];
+    await updateData(argsVitt);
+    return res.status(201).send("Updated white wine data")
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+}
+const getRed = async (req, res) => {
+  try {
+    console.log("Updating red wine data")
+    let argsRott = ['assortment', '--category', "Vin", '--subcategory', 'Rött vin'];
+    await updateData(argsRott);
+    return res.status(201).send("Updated red wine data")
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+}
+
+module.exports = {
+  getRed,
+  getRestWine,
+  getWhite
+};
--- backend/routes/updateRoutes.js
+++ backend/routes/updateRoutes.js
@@ -1,14 +1,16 @@
 //importing modules
 const express = require('express')
 const updateBeers = require('../jobs/updateBeers')
-const updateWines = require('../jobs/updateWines')
+const { getWhite, getRed, getRestWine } = require('../jobs/updateWines')
 const updateSpirits = require('../jobs/updateSpirits')
 const readDataFromJSON = require('../jobs/readFile')
 
 const router = express.Router()
 
 router.post('/beer', updateBeers)
-router.post('/wine', updateWines)
+router.post('/white', getWhite)
+router.post('/red', getRed)
+router.post('/rest', getRestWine)
 router.post('/spirit', updateSpirits);
 router.post('/old', readDataFromJSON);
 
