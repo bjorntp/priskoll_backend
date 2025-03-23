09c86111ebe10380b42d962fb17e83656f7421b8
Björn Tenje Persson <bjorn@tenje.se>
2025-03-23 09:06:51 +0100

    Easier deployment of frontend

--- frontend_systemet/deploy.sh
+++ frontend_systemet/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 # Define variables
 BRANCH="production_frontend"
-BUILD_DIR="dist"
+BUILD_DIR="frontend_systemet/dist"
 REPO=$(git rev-parse --show-toplevel)
 
 # Ensure we're in the frontend directory
@@ -14,13 +14,11 @@ git commit -m "Build files in dev"
 # Switch to the production branch
 git checkout "production_frontend"
 
-cd ..
-
 rm -r *
 
 # Copy built files to the branch root
-git checkout main -- frontend_systemet/dist
-mv frontend_systemet/dist/* .
+git checkout main -- $(BUILD_DIR)
+mv $(BUILD_DIR)/* .
 rm -r frontend_systemet/
 
 # Add and commit the changes
